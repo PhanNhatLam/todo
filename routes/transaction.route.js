4da81a027ea939f5eccce2cab7f745ce34e83c56
Glitch (helix-spot-zydeco) <none>
2020-06-10 08:12:47 +0000

    👧🏌 Updated with Glitch

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,7 +1,6 @@
 var express = require('express');
 
 var controller = require('../controllers/transaction.controller');
-var validate = require('../validate/transaction.validate');
 
 var router = express.Router();
 
@@ -19,6 +18,6 @@ router.get("/:id/delete", controller.delete);
 router.get("/:id", controller.view);
 
 // Status of transaction
-router.get("/:id/complete", validate.isComplete, controller.isComplete);
+router.get("/:id/complete", controller.isComplete);
 
 module.exports = router;
